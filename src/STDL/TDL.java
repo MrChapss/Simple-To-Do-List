dc38e27c7604ed4e5f089b4bd7bd3c261e898384
Chapss <palmakarsten@gmail.com>
2025-10-10 23:14:59 +0800

    fixing logic in container

--- src/STDL/TDL.java
+++ src/STDL/TDL.java
@@ -1,7 +1,18 @@
 package STDL;
+
+import java.util.Scanner;
+
 public class TDL {
 	public static void main(String[] args) {
-		IO.println("Hellow ninja");
-		// nag test ako para maintindihan yung git
+		Scanner input = new Scanner(System.in);
+		String userInput;
+		
+		do {
+			System.out.print("Input your to-do here: ");
+			userInput = input.nextLine();
+			String[] userTDL = new String[userInput]; // i'll fix this tomorrow (the container)
+			// iniisip ko pa kung paano ko magagawa yung add/delete ng walang limit sa array at loop
+			
+		} while (true);
 	}
 }
