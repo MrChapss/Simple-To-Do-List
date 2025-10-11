347e07e5d6565fbaa44121f482024e7c951ab5cc
Chapss <palmakarsten@gmail.com>
2025-10-12 04:04:12 +0800

    added some comments

--- src/STDL/TDL.java
+++ src/STDL/TDL.java
@@ -13,24 +13,27 @@ public class TDL {
 		// using do while to repeat the code
 		do {
 			System.out.printf("%nWelcome to TO-DO program!%n");
-			
+			// I use while loop for safety measure when exiting
 			while (true) {
 				System.out.print("Input your to-do here (input 'exit' to stop): ");
 				userTD = input.nextLine();
-				
+				// To prevent empty input from the user
 				if (userTD.isEmpty()) {
 					System.out.printf("%nPlease provide a to-do!%n%n");
 				}
-				
+				// To exit inputing to-do
 				if (userTD.equalsIgnoreCase("exit")) {
 					break;
 				} 
 				
+				// removing intentional/unintentional blank or empty to-do (need some fixing)
 				userTDAL.removeIf(inputs -> inputs.contentEquals(""));
+				// adding the user input into array list named userTDAL
 				userTDAL.add(userTD);
 				
 			}
 			// ilalagay ko to sa void statement since print lang naman gagawin neto
+			// print the output
 			System.out.printf("%n%nTO-DO LIST:%n");
 			// print the object inside of userTDAL Array List
 			for (int i=0; i<userTDAL.size(); i++) {
@@ -38,6 +41,7 @@ public class TDL {
 			}
 			
 			// lagay ko to sa return statement para maayos
+			// to ask the user if they want to try again the program
 			while (true) {
 				System.out.printf("%nDo you want to try again? (Yes/No): ");
 				userRetry = input.next();
