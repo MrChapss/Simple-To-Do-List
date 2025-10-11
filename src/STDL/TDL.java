04fd3934190bc823dc96718ee2f975b22ccb84c2
Chapss <palmakarsten@gmail.com>
2025-10-12 03:51:44 +0800

    almost finish

--- src/STDL/TDL.java
+++ src/STDL/TDL.java
@@ -1,18 +1,57 @@
 package STDL;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class TDL {
 	public static void main(String[] args) {
+		// initialize array list and scanner
 		Scanner input = new Scanner(System.in);
-		String userInput;
+		String userTD, userRetry;
+		ArrayList<String> userTDAL = new ArrayList<String>();
 		
+		// using do while to repeat the code
 		do {
-			System.out.print("Input your to-do here: ");
-			userInput = input.nextLine();
-			String[] userTDL = new String[userInput]; // i'll fix this tomorrow (the container)
-			// iniisip ko pa kung paano ko magagawa yung add/delete ng walang limit sa array at loop
+			System.out.printf("%nWelcome to TO-DO program!%n");
 			
-		} while (true);
+			while (true) {
+				System.out.print("Input your to-do here (input 'exit' to stop): ");
+				userTD = input.nextLine();
+				
+				if (userTD.isEmpty()) {
+					System.out.printf("%nPlease provide a to-do!%n%n");
+				}
+				
+				if (userTD.equalsIgnoreCase("exit")) {
+					break;
+				} 
+				
+				userTDAL.removeIf(inputs -> inputs.contentEquals(""));
+				userTDAL.add(userTD);
+				
+			}
+			// ilalagay ko to sa void statement since print lang naman gagawin neto
+			System.out.printf("%n%nTO-DO LIST:%n");
+			// print the object inside of userTDAL Array List
+			for (int i=0; i<userTDAL.size(); i++) {
+				System.out.println((i+1) + ". " + userTDAL.get(i));
+			}
+			
+			// lagay ko to sa return statement para maayos
+			while (true) {
+				System.out.printf("%nDo you want to try again? (Yes/No): ");
+				userRetry = input.next();
+				
+				if (!userRetry.equalsIgnoreCase("Yes") && !userRetry.equalsIgnoreCase("No")) {
+					System.out.printf("%nPlease choose from (Yes/No) only!%n");
+				}
+				
+				if (userRetry.equalsIgnoreCase("No") || userRetry.equalsIgnoreCase("Yes")) break;
+				
+			}
+			
+		} while (!userRetry.equalsIgnoreCase("No"));
+		
+		input.close();
 	}
 }
