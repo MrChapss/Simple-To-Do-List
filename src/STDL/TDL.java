3f0ddc7e2638b7900d48a992cf08976b389864fc
Chapss <palmakarsten@gmail.com>
2025-10-14 15:27:29 +0800

    methods ver

--- src/STDL/TDL.java
+++ src/STDL/TDL.java
@@ -7,55 +7,64 @@ public class TDL {
 	public static void main(String[] args) {
 		// initialize array list and scanner
 		Scanner input = new Scanner(System.in);
-		String userTD, userRetry;
 		ArrayList<String> userTDAL = new ArrayList<String>();
+		TDL methods = new TDL();
 		
 		// using do while to repeat the code
 		do {
 			System.out.printf("%nWelcome to TO-DO program!%n");
 			// I use while loop for safety measure when exiting
-			while (true) {
-				System.out.print("Input your to-do here (input 'exit' to stop): ");
-				userTD = input.nextLine();
-				// To prevent empty input from the user
-				if (userTD.isEmpty()) {
-					System.out.printf("%nPlease provide a to-do!%n%n");
-				}
-				// To exit inputing to-do
-				if (userTD.equalsIgnoreCase("exit")) {
-					break;
-				} 
-				
-				// removing intentional/unintentional blank or empty to-do (need some fixing)
-				userTDAL.removeIf(inputs -> inputs.contentEquals(""));
-				// adding the user input into array list named userTDAL
-				userTDAL.add(userTD);
-				
-			}
-			// ilalagay ko to sa void statement since print lang naman gagawin neto
+			methods.UATD(input, userTDAL);
+			
 			// print the output
-			System.out.printf("%n%nTO-DO LIST:%n");
-			// print the object inside of userTDAL Array List
-			for (int i=0; i<userTDAL.size(); i++) {
-				System.out.println((i+1) + ". " + userTDAL.get(i));
-			}
+			otdList(userTDAL);
 			
-			// lagay ko to sa return statement para maayos
 			// to ask the user if they want to try again the program
-			while (true) {
-				System.out.printf("%nDo you want to try again? (Yes/No): ");
-				userRetry = input.next();
-				
-				if (!userRetry.equalsIgnoreCase("Yes") && !userRetry.equalsIgnoreCase("No")) {
-					System.out.printf("%nPlease choose from (Yes/No) only!%n");
-				}
-				
-				if (userRetry.equalsIgnoreCase("No") || userRetry.equalsIgnoreCase("Yes")) break;
-				
+			URT(input);
+		// the do while is not working bruh
+		} while (true);
+		
+	}
+	
+	// print the to-do list
+	private static void otdList(ArrayList<String> userTDAL) {
+		System.out.printf("%nTO-DO LIST:%n");
+		for (int i=0; i<userTDAL.size(); i++) {
+			System.out.println((i+1) + ". " + userTDAL.get(i));
+		}
+	}
+	
+	// add to-do
+	private String UATD(Scanner userTD, ArrayList<String> uTDAL) {
+		while(true) {
+			System.out.print("Input your to-do here (input 'exit' to stop): ");
+			String ATD = userTD.nextLine();
+			// this is not working man when empty or intentional enter
+			if (ATD.isEmpty()){
+				return "Please input a to-do!";
 			}
 			
-		} while (!userRetry.equalsIgnoreCase("No"));
-		
-		input.close();
+			if (ATD.equalsIgnoreCase("exit")){
+				return ATD;
+			}
+			
+			uTDAL.removeIf(inputs -> inputs.trim().isEmpty());
+			uTDAL.add(ATD);
+		}
+	}
+	
+	// if user should try again
+	public static String URT(Scanner rt) {
+		while(true) {
+			System.out.printf("%nDo you want to try again? (Yes/No): ");
+			String choice = rt.next();
+			
+			if (!choice.equalsIgnoreCase("Yes") && !choice.equalsIgnoreCase("No")) {
+				return "%nPlease choose from (Yes/No) only!%n";
+			}
+			
+			if (choice.equalsIgnoreCase("No") || choice.equalsIgnoreCase("Yes")) return choice;
+		}
 	}
 }
+
