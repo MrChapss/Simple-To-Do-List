2806fe7c170d8a29996328296ecb884453396276
Chapss <palmakarsten@gmail.com>
2025-10-14 17:40:52 +0800

    checking if no bugs

--- src/STDL/TDL.java
+++ src/STDL/TDL.java
@@ -7,8 +7,11 @@ public class TDL {
 	public static void main(String[] args) {
 		// initialize array list and scanner
 		Scanner input = new Scanner(System.in);
-		String userTD, userRetry;
 		ArrayList<String> userTDAL = new ArrayList<String>();
+		// initialize variables
+		String userATD, userRetry;
+		int uRTD;
+		
 		
 		// using do while to repeat the code
 		do {
@@ -16,46 +19,58 @@ public class TDL {
 			// I use while loop for safety measure when exiting
 			while (true) {
 				System.out.print("Input your to-do here (input 'exit' to stop): ");
-				userTD = input.nextLine();
+				userATD = input.nextLine();
 				// To prevent empty input from the user
-				if (userTD.isEmpty()) {
+				if (userATD.isEmpty()) {
 					System.out.printf("%nPlease provide a to-do!%n%n");
 				}
 				// To exit inputing to-do
-				if (userTD.equalsIgnoreCase("exit")) {
-					break;
-				} 
-				
-				// removing intentional/unintentional blank or empty to-do (need some fixing)
-				userTDAL.removeIf(inputs -> inputs.contentEquals(""));
+				if (userATD.equalsIgnoreCase("exit")) break;
+				// remove the spaces entry
+				userTDAL.removeIf(inputs -> inputs.trim().isEmpty());
 				// adding the user input into array list named userTDAL
-				userTDAL.add(userTD);
-				
+				userTDAL.add(userATD);
 			}
-			// ilalagay ko to sa void statement since print lang naman gagawin neto
-			// print the output
-			System.out.printf("%n%nTO-DO LIST:%n");
-			// print the object inside of userTDAL Array List
-			for (int i=0; i<userTDAL.size(); i++) {
-				System.out.println((i+1) + ". " + userTDAL.get(i));
+			// to-do list
+			PTDL(userTDAL);
+			
+			// deleting to-do
+			while(true) {
+				System.out.printf("%nIf you want to delete a to-do, choose from the numbers.");
+				System.out.printf("%nInput '0' if nothing: ");
+				uRTD = input.nextInt();
+				// if user type 0 it will break the while loop
+				if ((uRTD) == 0) break;
+				// to remove the to-do based on their index position
+				userTDAL.remove(uRTD-1);
+				
+				PTDL(userTDAL);
 			}
 			
-			// lagay ko to sa return statement para maayos
 			// to ask the user if they want to try again the program
 			while (true) {
 				System.out.printf("%nDo you want to try again? (Yes/No): ");
 				userRetry = input.next();
-				
+				// if the user don't choose from (Yes/No)
 				if (!userRetry.equalsIgnoreCase("Yes") && !userRetry.equalsIgnoreCase("No")) {
-					System.out.printf("%nPlease choose from (Yes/No) only!%n");
+					System.out.printf("Please choose from (Yes/No) only!%n");
 				}
-				
+				// break the while loop when choose between Yes or No
 				if (userRetry.equalsIgnoreCase("No") || userRetry.equalsIgnoreCase("Yes")) break;
 				
 			}
-			
+		// To loop the program again if they input Yes
 		} while (!userRetry.equalsIgnoreCase("No"));
 		
 		input.close();
+	}
+	
+	// printing the output
+	public static void PTDL(ArrayList<String> userTDAL) {
+		System.out.printf("%nTO-DO LIST:%n");
+		// print the object inside of userTDAL Array List
+		for (int i=0; i<userTDAL.size(); i++) {
+			System.out.println((i+1) + ". " + userTDAL.get(i));
+		}
 	}
 }
